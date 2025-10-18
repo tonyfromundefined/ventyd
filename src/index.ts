285abacdffb3b3d9e72bfa2fd3b4b84c70bcf6fc
Tony Won <tony@daangn.com>
2025-10-19 02:01:01 +0900

    refactor: enhance type exports in `index.ts` by including `Repository`, `Reducer`, and `Storage` types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 export * as v from "valibot";
 
 // Functions
-export { createRepository } from "./createRepository";
-export { defineReducer } from "./defineReducer";
+export { createRepository, type Repository } from "./createRepository";
+export { defineReducer, type Reducer } from "./defineReducer";
 export { defineSchema } from "./defineSchema";
-export { defineStorage } from "./defineStorage";
+export { defineStorage, type Storage } from "./defineStorage";
 export { Entity } from "./Entity";
 
 // Type Helpers
